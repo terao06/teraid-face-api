3bef874310e986a2a33c6e99381729f148e635b2
Terao Yuta <nzdp4px@gmail.com>
2026-04-11 16:16:43 +0900

    ツール追加

--- app/core/messages.py
+++ app/core/messages.py
@@ -1,4 +1,4 @@
 class ValidationMessages:
     """バリデーションメッセージ定数"""
-    FACE_NOT_FOUND = "顔画像を認識できませんでした。"
-    MULTIPLE_FACES_DETECTED = "顔画像が複数存在します。"
+    FACE_NOT_FOUND = "顔画を認識できませんでした。"
+    MULTIPLE_FACES_DETECTED = "顔画が複数存在します。"
